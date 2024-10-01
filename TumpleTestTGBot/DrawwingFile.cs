42721389845df9ded20397e2e0ac64cb9f8622cf
Tumple <168741311+Tumple@users.noreply.github.com>
2024-10-01 20:21:02 +0300

    Add files via upload

---
+++ TumpleTestTGBot/DrawwingFile.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Data;
+using Telegram.Bot.Types;
+
+
+namespace TumpleTestTGBot
+{
+    class DrawwingFile
+    {
+        public static int Random()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 14);
+            return randomNumber;
+        }
+        public static string  DrawwingFile1(string username, string date) 
+        {
+            string fullname, group, reason;
+            string[] fullname1;
+            string filename = $"Результаты/{username}+{DateTime.Now.ToShortDateString()}.png";
+            try
+            {
+                using (StreamReader sr = new StreamReader($"Данные/{username}.txt"))
+                {
+                    fullname = sr.ReadLine();
+                    group = sr.ReadLine();
+                    reason = sr.ReadLine();
+                    fullname1 = fullname.Split(' ');
+                }
+
+
+                // Размеры A4 в пикселях при разрешении 300 DPI
+                int width = 2480;
+                int height = 3000;
+                // Загрузить 
+
+                System.Drawing.Text.PrivateFontCollection privateFontCollection = new System.Drawing.Text.PrivateFontCollection();
+                try
+                {
+                    privateFontCollection.AddFontFile(@$"Шрифты/{Random()}.ttf");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                
+                // Создаем объект Font, взяв из коллекции 0-й шрифт
+                System.Drawing.Font customFont = new System.Drawing.Font(privateFontCollection.Families[0], 65, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+                // Создание пустого Bitmap
+                using (Bitmap bitmap = new Bitmap(width, height))
+                {
+                    // Создание объекта Graphics для рисования на Bitmap
+                    using (System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(bitmap))
+                    {
+                        // Заливка фона белым цветом
+                        graphics.Clear(System.Drawing.Color.White);
+
+
+                        System.Drawing.Brush brush = Brushes.DarkBlue;
+
+                        // Текст для записи
+                        string lines =
+                                    $"Директору ГБПОУ\nКолледжа связи 54\nПавлюку И. А.\nот студента гр.{group}\n{fullname1[0]} {fullname1[1][0]}. {fullname1[2][0]}.";
+
+                        string lines2 =
+                                    $"      Я, {fullname}, студент группы {group} прошу Вас разрешить мне отсутствовать {date} на учебных занятиях по причине: {reason} \nОтветственность за изучение пропущенного материала беру на себя.";
+
+                        graphics.DrawString(lines, customFont, brush, 1400, 100);
+                        graphics.DrawString("Заявление", customFont, brush, 1200, 1000);
+                        DrawStringWithWordWrap(graphics, lines2, customFont, brush, new RectangleF(100, 1100, width - 200, height - 1200));
+                        
+                        //graphics.DrawString(lines2, customFont, brush, 100, 1100);
+                        using (Image image = Image.FromFile(@$"Росписи/{username}.png"))
+                        {
+                            graphics.DrawImage(image, 1500, 1800);
+                        }
+                        graphics.DrawString(DateTime.Today.ToShortDateString(), customFont, brush, 100, 2000);
+                        bitmap.Save(filename, ImageFormat.Png);
+
+                        graphics.Dispose();
+                    }
+                    bitmap.Dispose();
+                }
+            }
+            catch (Exception ex){ Console.WriteLine(ex); }
+            return filename;
+        }
+        public static void DrawStringWithWordWrap(Graphics graphics, string text, Font font, Brush brush, RectangleF layoutRectangle)
+        {
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Near;
+            format.LineAlignment = StringAlignment.Near;
+            format.Trimming = StringTrimming.Word;
+
+            graphics.DrawString(text, font, brush, layoutRectangle, format);
+        }
+    }
+
+}
+
---
+++ TumpleTestTGBot/Program.cs
@@ -0,0 +1,150 @@
+﻿using Telegram;
+using System.Drawing.Imaging;
+using System.Drawing;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using TumpleTestTGBot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using static System.Net.WebRequestMethods;
+using System.Threading;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.IO.Pipes;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+
+        DrawwingFile file = new DrawwingFile();
+
+        using var cts = new CancellationTokenSource();
+        Console.WriteLine("Введите токен бота");
+        var bot = new TelegramBotClient(Console.ReadLine(), cancellationToken: cts.Token);
+        Console.Clear();
+        var me = await bot.GetMeAsync();
+        bot.OnMessage += OnMessage;
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+        Console.WriteLine($"@{me.Username} начал работу в {DateTime.Now}\n");
+        Console.ResetColor();
+        Console.ReadLine();
+        cts.Cancel(); // stop the bot
+
+        // method that handle messages received by the bot:
+        async Task OnMessage(Message msg, UpdateType type)
+        {
+            if(!System.IO.File.Exists($"Данные/{msg.Chat.Username.ToString()}.txt"))
+            {
+                System.IO.File.Create($"Данные/{msg.Chat.Username.ToString()}.txt").Close();
+            }
+
+
+
+            if (msg.Type.ToString() == "Photo")
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"Получено фото\nОт ({msg.Chat.Id}) @{msg.Chat.Username.ToString()}\nВремя отправки: {DateTime.Now}\n");
+                    Console.ResetColor();
+                    var fileId = msg.Photo.Last().FileId;
+                    var fileInfo = await bot.GetFileAsync(fileId);
+                    var filePath = fileInfo.FilePath;
+                    await using Stream fileStream = System.IO.File.Create(@$"Росписи/{msg.Chat.Username.ToString()}.png");
+                    //await bot.GetInfoAndDownloadFileAsync(fileId, fileStream);
+                    await bot.DownloadFileAsync(filePath, fileStream, cancellationToken: cts.Token);
+                    fileStream.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                await bot.SendTextMessageAsync(msg.Chat, "Фотку получил!\nТеперь отправь свое ФИО\nПример:");
+                await bot.SendTextMessageAsync(msg.Chat, "1 Булдаков Владислав Александрович");//---------------------------------------------- 1
+
+            }
+            else if(msg.Type.ToString() == "Text")
+            {
+                var msgcheck = msg.Text.Split(' ');
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine($"Сообщение '{msg.Text}'\nОт ({msg.Chat.Id}) @{msg.Chat.Username.ToString()}\nВремя отправки: {DateTime.Now}\n");
+                Console.ResetColor();
+                switch (msgcheck[0])
+                {
+                    case "/start":
+                        await bot.SendTextMessageAsync(msg.Chat, "Привет\nЯ напишу тебе заявление.\nЧтобы продолжить нажми на кнопку",
+                            replyMarkup: new ReplyKeyboardMarkup(true)
+                            .AddButtons("Заявление"));
+                        break;
+                    case "Заявление":
+                        await bot.SendTextMessageAsync(msg.Chat, "Мне нужна будет твоя информация!\nДля начала отправь мне фото твоей росписи!");
+                        await bot.SendTextMessageAsync(msg.Chat, "Пример", replyMarkup: new ReplyKeyboardRemove());
+                        FileStream fileStream = System.IO.File.Open((@"роспись.png"), FileMode.Open);
+                        await bot.SendPhotoAsync(
+                            chatId: msg.Chat.Id,
+                            photo: InputFile.FromStream(fileStream),
+                            cancellationToken: cts.Token);
+
+                        break;
+                    case "1":
+                        
+                        using (StreamWriter sr = new StreamWriter($"Данные/{msg.Chat.Username.ToString()}.txt", false))
+                        {
+                            await sr.WriteLineAsync(msg.Text.Substring(2));
+                        }
+                        await bot.SendTextMessageAsync(msg.Chat, "Теперь отправь свою группу\nПример:");
+                        await bot.SendTextMessageAsync(msg.Chat, "2 3-ИСП9-34");
+                        break;
+                    case "2":
+                        using (StreamWriter sr = new StreamWriter($"Данные/{msg.Chat.Username.ToString()}.txt", true))
+                        {
+                            await sr.WriteLineAsync(msg.Text.Substring(2));
+                        }
+                        await bot.SendTextMessageAsync(msg.Chat, "Теперь отправь причину отсутствия\nПример:");
+                        await bot.SendTextMessageAsync(msg.Chat, "3 Семейные обстоятельства");
+                        break;
+                    case "3":
+                        using (StreamWriter sr = new StreamWriter($"Данные/{msg.Chat.Username.ToString()}.txt", true))
+                        {
+                            await sr.WriteLineAsync(msg.Text.Substring(2));
+                        }
+                        await bot.SendTextMessageAsync(msg.Chat, "Теперь отправь дату отсутствия\nПример:");
+                        await bot.SendTextMessageAsync(msg.Chat, "4 01.05.2013");
+                        break;
+                    case "4":
+                        await bot.SendPhotoAsync(
+                        chatId: msg.Chat.Id,
+                        photo: InputFile.FromStream(System.IO.File.Open(DrawwingFile.DrawwingFile1(msg.Chat.Username.ToString(), msg.Text.Substring(2)), FileMode.Open)),
+                        cancellationToken: cts.Token);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Заявление отправлено для @{msg.Chat.Username.ToString()}\n");
+                        Console.ResetColor();
+                        await bot.SendTextMessageAsync(msg.Chat, "Готово!\nТеперь отправляй своему куратору и кайфуй");
+                        await bot.SendTextMessageAsync(msg.Chat, "Чтобы начать работать с ботом отправьте /start",
+                            replyMarkup: new ReplyKeyboardMarkup(true).AddButtons("/start"));
+                        break;
+                    default:
+                            await bot.SendTextMessageAsync(msg.Chat, "Напиши как в примере плиз");
+                        
+                        break;
+                }
+
+            }
+            else
+            {
+                await bot.SendTextMessageAsync(msg.Chat, "Я принимаю только фото и текст");
+            }
+
+            // comit
+        }
+    }
+}
